0c8a9a9f6f3cb5aa010af9ff12b77bec4876c459
Tanmay <tanmayarora382006@gmail.com>
2025-07-13 19:28:01 +0530

    loading page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Loading from './components/Loading/Loading'
+
+function App() {
+ 
+
+  return (
+    <>
+     <Loading/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Loading/Loading.jsx
@@ -0,0 +1,59 @@
+import React, { useRef } from 'react';
+import { gsap } from 'gsap';
+import { useGSAP } from '@gsap/react';
+import TextPlugin from 'gsap/TextPlugin';
+
+gsap.registerPlugin(TextPlugin);
+
+const Loading = () => {
+  const titleRef = useRef();
+  const subtitleRef = useRef();
+
+  useGSAP(() => {
+    gsap.fromTo(
+      titleRef.current,
+      { opacity: 0,y:-10, scale: 0.9 },
+      { opacity: 1, y:-10, scale: 1.3, duration: 0.9, ease: 'power2.out' }
+    );
+    gsap.fromTo(
+      subtitleRef.current,
+      { opacity: 0, y: 20 },
+      { opacity: 1, y: 0, duration: 1.4, delay: 1, ease: 'power2.out', text:'BITS Pilani'}
+      
+    );
+    gsap.fromTo('#loadingCircle',{
+      x:-600,
+      opacity:0
+    
+    },{x:0,opacity:1,duration:0.8,ease:'power2.out',delay:0.1})
+  }, []);
+
+  return (
+    <section
+      className="w-full h-screen flex flex-col justify-center items-center bg-[#302c72] relative"
+    >
+      {/* SVG Circle as background */}
+      <svg width="1000" height="500" style={{ zIndex: 1, position: 'absolute', left: '50%', top: '50%', transform: 'translate(-73%, -50%)' }}>
+        <circle id='loadingCircle' cx="50%" cy="50%" r="250" fill="white" />
+      </svg>
+      {/* Title always above SVG */}
+      <h1
+        ref={titleRef}
+        className="font-[sora] text-9xl text-white font-bold"
+        style={{ letterSpacing: '0.05em', position: 'relative', zIndex: 2 }}
+      >
+        <span style={{ color: '#302c72' }}>IIC</span>hE
+      </h1>
+      <h3
+        ref={subtitleRef}
+        className="font-[sora] text-5xl text-white translate-x-40"
+        style={{ position: 'relative', zIndex: 2 }}
+      >
+       
+       
+      </h3>
+    </section>
+  );
+};
+
+export default Loading;
